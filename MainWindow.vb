21ac77544bfa40f60aa37c46d3af3865739a828a
Juan Dueñas <jcduenast@unal.edu.co>
2022-06-19 20:24:05 +0200

    Initial setup

---
+++ MainWindow.vb
@@ -0,0 +1,10 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Imports System.ComponentModel
+
+Public Class MainWindow
+    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
