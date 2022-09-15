306847662aa81f1171cdcfefab7561b7d8436b40
Juan Dueñas <38484459+jcduenast@users.noreply.github.com>
2022-09-15 17:08:19 +0200

    waves updated

--- speaker/speaker_neu/speaker_neu.ino
+++ speaker/speaker_neu/speaker_neu.ino
@@ -6,83 +6,81 @@ int count = 1;
 int i;
 
 void setup() {
-
-Serial.begin(115200);
-ledcSetup(channel, freq, resolution);
-ledcAttachPin(17, channel);
+  Serial.begin(115200);
+  ledcSetup(channel, freq, resolution);
+  ledcAttachPin(17, channel);
+  pinMode(15, INPUT);
 }
 
 void loop() {
-
-
-// Read and interpret serial input
-if (Serial.available() > 0) {
-// read serial input, if available
-String receivedString = "";
-while (Serial.available() > 0) {
-// read serial input
-int receivedByte = Serial.read();
-receivedString += (char)receivedByte;
-}
-if (receivedString.length() > 1) {
-    int dlmIdx = receivedString.indexOf(" ");
-    if (dlmIdx != -1) {
+  if (digitalRead(15)){
+    //turn speaker on
+    Serial.println("speaker is on");
+    ledcWriteTone(channel, freq);
+    delay(200);
+    ledcWrite(channel, 0);
+  }else{
+//    //turn speaker off
+//    ledcWrite(channel, 0);
+//    Serial.println("speaker is off");
+  }
+  // Read and interpret serial input
+  if (Serial.available() > 0) {
+    // read serial input, if available
+    String receivedString = "";
+    
+    while (Serial.available() > 0) {
+      // read serial input
+      int receivedByte = Serial.read();
+      receivedString += (char)receivedByte;
+    }
+    if (receivedString.length() > 1) {
+      int dlmIdx = receivedString.indexOf(" ");
+      if (dlmIdx != -1) {
         // interpret string prior to delimiter as command and after delimiter as parameter
         String cmd = receivedString.substring(0, dlmIdx);
         String param = receivedString.substring(dlmIdx + 1, receivedString.length() - 1);
         
         if (cmd == "freq") {
-            // Set set frequency of speaker
-            freq = param.toFloat();
-            Serial.print("frequency of speaker set to ");
-            Serial.print(freq);
-            Serial.println("Hz");
-            } 
-            
-         else if (cmd == "speaker" && param == "on") {
-                //turn speaker on
-                Serial.println("speaker is on");
-                ledcWriteTone(channel, freq);
-                }
-
-         else if (cmd == "speaker" && param == "off") {
-                //turn speaker off
-                ledcWrite(channel, 0);
-                Serial.println("speaker is off");
-                }
-                 
-         else if (cmd == "duration"){
-                // Set tone duration
-                duration = param.toFloat();
-                Serial.print("duration set to ");
-                Serial.print(duration);
-                Serial.println("ms");
-                }
-                
-         else if (cmd == "pulse" && param == "on") {
-                //turn speaker on
-                Serial.print("pulsing for ");
-                Serial.println(duration);
-                Serial.print(count);
-                Serial.println(" times");
-                for(i=0; i<count; i++) {
-                ledcWriteTone(channel, freq);
-                delay(duration);
-                ledcWrite(channel,0);
-                delay(1000);
-                }
-                }
-
-         else if (cmd == "count"){
-                // Set number of pulses
-                count = param.toFloat();
-                Serial.print("pulse count set to ");
-                Serial.println(count);
-                }
-                
-                    
-            }
-        }
-delay(1000);
-}
+          // Set set frequency of speaker
+          freq = param.toFloat();
+          Serial.print("frequency of speaker set to ");
+          Serial.print(freq);
+          Serial.println("Hz");
+        } else if (cmd == "speaker" && param == "on") {
+          //turn speaker on
+          Serial.println("speaker is on");
+          ledcWriteTone(channel, freq);
+        } else if (cmd == "speaker" && param == "off") {
+          //turn speaker off
+          ledcWrite(channel, 0);
+          Serial.println("speaker is off");
+        } else if (cmd == "duration"){
+          // Set tone duration
+          duration = param.toFloat();
+          Serial.print("duration set to ");
+          Serial.print(duration);
+          Serial.println("ms");
+        } else if (cmd == "pulse" && param == "on") {
+          //turn speaker on
+          Serial.print("pulsing for ");
+          Serial.println(duration);
+          Serial.print(count);
+          Serial.println(" times");
+          for(i=0; i<count; i++) {
+            ledcWriteTone(channel, freq);
+            delay(duration);
+            ledcWrite(channel,0);
+            delay(1000);
+          }
+        } else if (cmd == "count"){
+          // Set number of pulses
+          count = param.toFloat();
+          Serial.print("pulse count set to ");
+          Serial.println(count);
+        }                      
+      }
+    }
+//    delay(1000);
+  }
 }
